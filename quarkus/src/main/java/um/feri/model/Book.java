2aac11ef414447456420b95300153ce7fe4f0048
Nik Kovačević <nik.kovacevic@student.um.si>
2024-09-15 15:54:52 +0200

    Models

---
+++ quarkus/src/main/java/um/feri/model/Book.java
@@ -0,0 +1,36 @@
+package um.feri.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "books")
+public class Book extends PanacheEntity {
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false)
+    private Integer yearOfRelease;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Author author;
+}
